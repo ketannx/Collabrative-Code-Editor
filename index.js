9712b567db030892217a7d79d6493ab18b743ca2
ketan996 <scodesofiicial@gmail.com>
2025-06-09 22:45:53 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const connectDB = require('./db/db');
+
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Basic route
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to the Collaborative Code Editor API' });
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: 'Something went wrong!' });
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
